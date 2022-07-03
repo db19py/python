bfc75c7f23c0166e749ae53a6fe57165cef55af7
db19py <107420208+db19py@users.noreply.github.com>
2022-07-03 17:15:03 +0530

    Add files via upload

---
+++ june25_task2&4.py
@@ -0,0 +1,22 @@
+'''a = [1,2,3,4,[100,101,102,"Computer_science"],200,203]
+
+Extract
+#science
+#computer'''
+
+a = [1,2,3,4,[100,101,102,"Computer_science"],200,203]
+print(a[4][3][9:16])
+print(a[4][3][0:8])
+
+
+'''Li1 = [2,3,"python","hello",4,5,0]  
+
+Extract
+#ll
+#thon'''
+
+Li1 = [2,3,"python","hello",4,5,0]
+print(Li1[3][2:4])
+print(Li1[2][2:6])
+
+
