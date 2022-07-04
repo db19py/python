d08704d8f981409b83659ba3f90028209c9620c6
db19py <107420208+db19py@users.noreply.github.com>
2022-07-04 09:16:48 +0530

    Add files via upload

---
+++ July3-tasks/for_range_task1.py
@@ -0,0 +1,21 @@
+for i in range(76):
+    if (i!=0) and (i%5==0):
+        print(i)
+    else:
+        pass
+
+for i in range(72):
+    if (i!=0) and (i%8==0):
+        print(i)
+    else:
+        pass
+for i in range(75,1,-1):
+    if (i!=0) and (i%5==0):
+        print(i)
+    else:
+        pass
+for i in range(96,71,-1):
+    if (i!=0) and (i%8==0):
+        print(i)
+    else:
+        pass
