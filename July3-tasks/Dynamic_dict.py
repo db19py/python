00027de96b39325b5b98f09ae3f244acb591e18e
db19py <107420208+db19py@users.noreply.github.com>
2022-07-08 22:23:37 +0530

    Add files via upload

---
+++ July3-tasks/Dynamic_dict.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the number:"))
+b=dict()
+for i in range(a):
+    c=input("Enter the value:")
+    b[i]=c
+print(b)
+
---
+++ July3-tasks/Dynamiclist.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the number:"))
+b=[]
+for i in range(a):
+    c=input("Enter the element:")
+    b.append(c)
+print(b)
+    
