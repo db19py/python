903d7b4c21b9dd8ddce21a6bf35b63a12800facb
db19py <107420208+db19py@users.noreply.github.com>
2022-07-09 19:08:29 +0530

    Add files via upload

--- July3-tasks/task4.py
+++ July3-tasks/task4.py
@@ -1,7 +1,11 @@
 num1=int(input("Enter the first number:"))
 num2=int(input("Enter the second number:"))
-for i in range(num1,num2):
-    if i%8==0:
-        print(i)
-    else:
-        pass
+if num1>num2:
+    for i in range(num1,num2,-1):
+        if i%8==0:
+            print(i)
+    
+else:
+    for i in range(num1,num2):
+        if i%8==0:
+            print(i)
